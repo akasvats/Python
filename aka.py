a92dc88dce209665899b4b26808e695703ccfdf6
Vats Vaibhaw <akbaakashkumar000@gmail.com>
2024-09-24 10:46:04 +0530

    bb

---
+++ aka.py
@@ -0,0 +1,16 @@
+name="Aakash Singh"
+age=18
+salary=2599.89
+
+print("My name is "+ name +" and salary is",salary ," and age is", age )
+print(f"My name is {name}  and salary is {salary} and age is {age}" )
+a=5
+b=10
+c=a+b
+print("Sum is",c)
+#Logical operator in python
+z=int(input("Input the age"))
+if z>18:
+    print( "is eligible")
+else: 
+    print( "is not eligible")
